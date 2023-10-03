5d480f9bf09630431568f3a4127aa062de5d081f
Fazh <fahrialhamda31@gmail.com>
2023-10-03 15:20:54 +0700

    Commit Pertama

---
+++ scrip.js
@@ -0,0 +1,9 @@
+var body = document.getElementById('body');
+
+function darkMode(){
+    if(body.classList.contains('dark-mode')){
+        body.classList.remove('dark-mode')
+    }else {
+        body.classList.add('dark-mode')
+    }
+}
